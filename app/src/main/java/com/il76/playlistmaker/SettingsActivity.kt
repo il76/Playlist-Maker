792bb7092e58603e1a23f88684b52d8777ae7f56
il <igurylev@yandex.ru>
2024-08-03 11:06:51 +0300

    Тёмная тема. Пока частичной копипастой, в будущем можно отрефакторить.

--- app/src/main/java/com/il76/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/il76/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,13 @@
 package com.il76.playlistmaker
 
 import android.os.Bundle
-import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +19,22 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonBack = findViewById<ImageView>(R.id.settings_back_button)
+        buttonBack.setOnClickListener {
+            this.finish()
+        }
+
+        //обрабатываем переключатель темы
+        val switcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+        switcher.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                // переключаем системную тему
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 
-    fun btnBackClick(view: View) {
-        this.finish()
-    }
 }
