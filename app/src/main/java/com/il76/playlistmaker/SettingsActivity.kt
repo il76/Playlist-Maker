16d073a92fea52bd3c3146ae4358aca3b083a7ec
il <igurylev@yandex.ru>
2024-08-27 18:06:44 +0300

    Интенты, но пока не все работают правильно

--- app/src/main/java/com/il76/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/il76/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,10 @@
 package com.il76.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -8,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +30,49 @@ class SettingsActivity : AppCompatActivity() {
             this.finish()
         }
 
+
+        val buttonShare = findViewById<MaterialTextView>(R.id.SettingsShareApp)
+        buttonShare.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_app_text))
+            intent.setType("text/plain")
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, getString(R.string.action_not_supported), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val buttonWrite = findViewById<MaterialTextView>(R.id.SettingsWriteSupport)
+        buttonWrite.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "*/*"
+                //setData (Uri.parse("mailto:")) //нам нужны только почтовые приложения. Не работает?
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("il@9111.ru"))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.settings_email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_email_text))
+
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, getString(R.string.action_not_supported), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val buttonUA = findViewById<MaterialTextView>(R.id.SettingsUserAgreement)
+        buttonUA.setOnClickListener {
+            val webpage: Uri = Uri.parse(getString(R.string.settings_ua_link))
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, getString(R.string.action_not_supported), Toast.LENGTH_SHORT).show()
+            }
+        }
+
         //обрабатываем переключатель темы
-        val switcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+        val switcher = findViewById<SwitchMaterial>(R.id.SettingsThemeSwitcher)
         switcher.setOnCheckedChangeListener {_, isChecked ->
             if (isChecked) {
                 //setTheme(R.style.)
