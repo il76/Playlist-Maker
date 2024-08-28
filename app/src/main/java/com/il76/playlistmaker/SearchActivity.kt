1a163939d36b1b0ba203897f80e66e6d781ca2b1
il <igurylev@yandex.ru>
2024-08-28 17:53:01 +0300

    Вёрстка поисковой формы

--- app/src/main/java/com/il76/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/il76/playlistmaker/SearchActivity.kt
@@ -1,11 +1,17 @@
 package com.il76.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import org.w3c.dom.Text
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +28,36 @@ class SearchActivity : AppCompatActivity() {
         buttonBack.setNavigationOnClickListener {
             this.finish()
         }
+
+
+        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.search_icon_clear)
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val searchTextWatcher = object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // не нужно
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    
+                } else {
+                    // показываем кнопку сброса
+                }
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // не нужно
+            }
+        }
+
+        inputEditText.addTextChangedListener(searchTextWatcher)
+
+
 
     }
 }
