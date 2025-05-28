dc982f74131dbfce8c843b3059f5d8a654eeb641
il <igurylev@yandex.ru>
2025-05-28 20:43:49 +0300

    Версии компонентов. Восстановление поискового запроса при повороте.

--- app/src/main/java/com/il76/playlistmaker/search/ui/SearchFragment.kt
+++ app/src/main/java/com/il76/playlistmaker/search/ui/SearchFragment.kt
@@ -1,65 +1,38 @@
 package com.il76.playlistmaker.search.ui
 
-import android.content.IntentFilter
-import android.net.ConnectivityManager
-import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
@@ -67,36 +40,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
-import androidx.core.content.getSystemService
-import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import coil.compose.rememberAsyncImagePainter
 import com.il76.playlistmaker.R
-import com.il76.playlistmaker.databinding.FragmentSearchBinding
-import com.il76.playlistmaker.player.ui.PlayerFragment
 import com.il76.playlistmaker.search.domain.models.Track
-import com.il76.playlistmaker.settings.ui.SettingsViewModel
 import com.il76.playlistmaker.ui.shared.ErrorImageText
 import com.il76.playlistmaker.ui.shared.TrackList
-import com.il76.playlistmaker.utils.InternetBroadcastReceiver
-import com.il76.playlistmaker.utils.debounce
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.compose.koinViewModel
-import org.koin.androidx.compose.viewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 //class SearchFragment: Fragment() {
 //
@@ -310,7 +261,7 @@ fun SearchScreen(navController: NavController) {
     val context = LocalContext.current
     val uiState by viewModel.uiState.observeAsState()
 
-    val currentQuery = remember { mutableStateOf("") }
+    var currentQuery by rememberSaveable { mutableStateOf("") }
 
     // Показ Toast при получении события
     LaunchedEffect (Unit) {
@@ -333,7 +284,7 @@ fun SearchScreen(navController: NavController) {
         SearchTextField(
             modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.pdg_root), vertical = 8.dp),
             onSearchTextChanged = {
-                currentQuery.value = it
+                currentQuery = it
                 viewModel.searchDebounce(it)
             }
         )
@@ -353,7 +304,7 @@ fun SearchScreen(navController: NavController) {
                     SearchState.ErrorStatus.ERROR_NET -> {
                         ErrorImageText(R.drawable.search_network_error, R.string.search_network_error)
                         Button(
-                            onClick = {viewModel.doSearch(currentQuery.value)},
+                            onClick = {viewModel.doSearch(currentQuery)},
                             modifier = Modifier
                                 .padding(16.dp).align(Alignment.CenterHorizontally)
                         ) {
@@ -398,7 +349,7 @@ fun SearchTextField(
     hint: String = stringResource(id = R.string.button_search),
     onSearchTextChanged: (String) -> Unit
 ) {
-    var searchText by remember { mutableStateOf("") }
+    var searchText by rememberSaveable { mutableStateOf("") }
 
     Box(
         modifier = modifier
