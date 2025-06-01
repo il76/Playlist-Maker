abc7584bee7b1ca18d9e62ac737430fb90f4296b
il <igurylev@yandex.ru>
2025-06-01 14:17:01 +0300

    Добавил пейджер для эффекта свайпа

--- app/src/main/java/com/il76/playlistmaker/media/ui/MediaScreen.kt
+++ app/src/main/java/com/il76/playlistmaker/media/ui/MediaScreen.kt
@@ -1,5 +1,6 @@
 package com.il76.playlistmaker.media.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -18,9 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,29 +35,45 @@ import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.il76.playlistmaker.R
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MediaScreen(navController: NavController) {
     val viewModel: MediaViewModel = koinViewModel()
     val context = LocalContext.current
-    val backgroundColor = remember {ContextCompat.getColor(context, R.color.background_secondary)}
+    val backgroundColor = remember { ContextCompat.getColor(context, R.color.background_secondary) }
     val textColor = remember { ContextCompat.getColor(context, R.color.settings_text) }
     val buttonTextColor = remember { ContextCompat.getColor(context, R.color.background_secondary) }
     val buttonBackgroundColor = remember { ContextCompat.getColor(context, R.color.back_icon_fill) }
 
     val currentTab by viewModel.currentTab.collectAsState()
+    val pagerState = rememberPagerState(initialPage = currentTab) {2}
+
+    // Синхронизируем PagerState с ViewModel
+    LaunchedEffect(pagerState.currentPage) {
+        viewModel.selectTab(pagerState.currentPage)
+    }
+
+    // Синхронизируем ViewModel с PagerState
+    LaunchedEffect(currentTab) {
+        launch {
+            pagerState.animateScrollToPage(currentTab)
+        }
+    }
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(
-                    text = stringResource(R.string.button_media),
-                    style = MaterialTheme.typography.titleLarge.copy(
-                        color = Color(textColor)
+                title = {
+                    Text(
+                        text = stringResource(R.string.button_media),
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            color = Color(textColor)
+                        )
                     )
-                ) },
+                },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color(backgroundColor)
                 ),
@@ -62,7 +84,6 @@ fun MediaScreen(navController: NavController) {
             TabRow(
                 selectedTabIndex = currentTab,
                 indicator = { tabPositions ->
-                    // Используем Box для кастомного индикатора
                     Box(
                         modifier = Modifier
                             .tabIndicatorOffset(tabPositions[currentTab])
@@ -72,7 +93,6 @@ fun MediaScreen(navController: NavController) {
                     )
                 },
                 divider = {
-                    // Убираем дефолтный разделитель
                     HorizontalDivider(modifier = Modifier.height(0.dp))
                 },
                 containerColor = Color(buttonBackgroundColor),
@@ -99,9 +119,18 @@ fun MediaScreen(navController: NavController) {
                     )
                 }
             }
-            when (currentTab) {
-                0 -> TracksScreen(navController)
-                1 -> PlaylistsScreen(navController)
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(align = Alignment.Top) // Прижимаем контент к верху
+                    .weight(1f)
+            ) { page ->
+                when (page) {
+                    0 -> TracksScreen(navController)
+                    1 -> PlaylistsScreen(navController)
+                }
             }
         }
     }
